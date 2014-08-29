60d42efc93ed35bd826868783029b229781f2dd1
brantje <brantje@gmail.com>
2014-08-29 11:19:08 +0200

    trying to make an API route, currently not working -> login screen

---
+++ controller/locationcontroller.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * ownCloud - maps
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Sander Brand <brantje@gmail.com>
+ * @copyright Sander Brand 2014
+ */
+
+namespace OCA\Maps\Controller;
+
+use \OCP\IRequest;
+use \OCP\AppFramework\Http\TemplateResponse;
+use \OCP\AppFramework\Controller;
+
+
+class LocationController extends Controller {
+
+	private $userId;
+	private $cacheManager;
+	public function __construct($appName, IRequest $request, $userId) {
+		parent::__construct($appName, $request);
+		$this -> userId = $userId;
+	}
+
+	/**
+	 * CAUTION: the @Stuff turn off security checks, for this page no admin is
+	 *          required and no CSRF check. If you don't know what CSRF is, read
+	 *          it up in the docs or you might create a security hole. This is
+	 *          basically the only required method to add this exemption, don't
+	 *          add it to any other method if you don't exactly know what it does
+	 *
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 * @CORS
+	 */
+	public function update() {
+		$params = array('user' => $this -> userId);
+		return new TemplateResponse('maps', 'main', $params);
+		// templates/main.php
+	}
+
+}

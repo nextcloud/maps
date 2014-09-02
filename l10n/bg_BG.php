07eb0ad8a35c60114a54c558848fa3f15e715df3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-02 00:47:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/bg_BG.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Maps" => "Карти",
+"left" => "ляво",
+"right" => "дясно"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/fi_FI.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Maps" => "Kartat",
+"left" => "vasen",
+"right" => "oikea"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

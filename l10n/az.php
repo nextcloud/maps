30d0327e8590ce7b7ecfa5df4496ef4b0059d01b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-12 03:30:04 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Maps" => "Xəritələr",
+"left" => "Sol",
+"right" => "Sağ"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/ja.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Maps" => "地図",
+"left" => "左",
+"right" => "右"
+);
+$PLURAL_FORMS = "nplurals=1; plural=0;";

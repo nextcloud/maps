7fbbd23b2fad3637c3d27ff1e9e2e96771eafaeb
Thomas Müller <thomas.mueller@tmit.eu>
2014-12-05 09:34:47 +0100

    fix travis execution

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,12 +1,8 @@
 <?php
 
-global $RUNTIME_NOAPPS;
-$RUNTIME_NOAPPS = true;
-
 define('PHPUNIT_RUN', 1);
 
-require_once __DIR__.'/../../../lib/base.php';
-//require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../../../../lib/base.php';
 
 if(!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');

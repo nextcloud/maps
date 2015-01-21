7d640d3d03d2b6bd76b4f90ce6bb35582478c77d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-21 00:47:12 -0500

    [tx-robot] updated from transifex

---
+++ l10n/hr.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "maps",
+    {
+    "Maps" : "Karte",
+    "left" : "lijevo",
+    "right" : "Desno"
+},
+"nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;");

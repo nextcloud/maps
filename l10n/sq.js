405d829c2b50a2b6ca0186344ad17088abbbfad8
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-14 00:47:23 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sq.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "maps",
+    {
+    "Maps" : "Harta",
+    "left" : "majtas",
+    "right" : "djathtas"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/sq.php
@@ -0,0 +1,7 @@
+<?php
+$TRANSLATIONS = array(
+"Maps" => "Harta",
+"left" => "majtas",
+"right" => "djathtas"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

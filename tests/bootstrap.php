4bc4ec3c44596ff12ef325d6073a782ddbcf8bf1
Julien Veyssier <eneiluj@posteo.net>
2019-04-01 01:10:18 +0200

    tests are back from the darkness

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,26 @@
 <?php
+/**
+ * Nextcloud - maps
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ */
+define('PHPUNIT_RUN', 1);
 
-if (!defined('PHPUNIT_RUN')) {
-    define('PHPUNIT_RUN', 1);
-}
+// ugly hack to fix issues with template code using static code
+$_SERVER['REQUEST_URI'] = '/index.php/apps/maps/';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
 
 require_once __DIR__.'/../../../lib/base.php';
 
-// Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
-\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-
-// Fix for "Autoload path not allowed: .../maps/tests/testcase.php"
-\OC_App::loadApp('maps');
-
-if(!class_exists('PHPUnit_Framework_TestCase')) {
-    require_once('PHPUnit/Autoload.php');
+if (version_compare(implode('.', \OCP\Util::getVersion()), '8.2', '>=')) {
+    \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+    \OC_App::loadApp('maps');
 }
+
+//if(!class_exists('PHPUnit_Framework_TestCase')) {
+//	require_once('PHPUnit/Autoload.php');
+//}
 
 OC_Hook::clear();

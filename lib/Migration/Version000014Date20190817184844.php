2d77e9428557a4ee1e92ada36a8a0c19cd62ce6a
Julien Veyssier <eneiluj@posteo.net>
2019-08-17 18:55:45 +0200

    fix bug with PostgreSQL when setting long favorite name
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

---
+++ lib/Migration/Version000014Date20190817184844.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Maps\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+use OCP\IDBConnection;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version000014Date20190817184844 extends SimpleMigrationStep {
+
+	protected $db;
+
+	public function __construct(IDBConnection $connection) {
+		$this->db = $connection;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable('maps_favorites')) {
+			$table = $schema->getTable('maps_favorites');
+			$table->changeColumn('name', [
+				'notnull' => false,
+				'length' => 255,
+			]);
+		}
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}

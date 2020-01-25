5874059c992f6b6413efebc1275ed760b2217963
Arne Hamann <kontakt+github@arne.email>
2020-01-25 12:04:02 +0100

    Untested draft
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

--- js/script.js
+++ js/script.js
@@ -251,6 +251,42 @@
                         attribution: attribSat
                     });
                     //mapController.controlLayers.addBaseLayer(mapController.baseLayers['Mapbox satellite'], 'Mapbox satellite');
+                    mapController.map.addLayer(L.mapboxGL(
+                        {
+                            'accessToken': optionsValues.mapboxAPIKEY,
+                            'id': '3d-buildings',
+                            'source': 'composite',
+                            'source-layer': 'building',
+                            'filter': ['==', 'extrude', 'true'],
+                            'type': 'fill-extrusion',
+                            'minzoom': 15,
+                            'paint': {
+                                'fill-extrusion-color': '#aaa',
+
+                                // use an 'interpolate' expression to add a smooth transition effect to the
+                                // buildings as the user zooms in
+                                'fill-extrusion-height': [
+                                    'interpolate',
+                                    ['linear'],
+                                    ['zoom'],
+                                    15,
+                                    0,
+                                    15.05,
+                                    ['get', 'height']
+                                ],
+                                'fill-extrusion-base': [
+                                    'interpolate',
+                                    ['linear'],
+                                    ['zoom'],
+                                    15,
+                                    0,
+                                    15.05,
+                                    ['get', 'min_height']
+                                ],
+                                'fill-extrusion-opacity': 0.6
+                            }
+                        }
+                    ));
                 }
                 if (optionsValues.hasOwnProperty('tileLayer')) {
                     mapController.changeTileLayer(optionsValues.tileLayer);

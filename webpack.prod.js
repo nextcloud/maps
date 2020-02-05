75540fcc6362ce6730316d1fa60aa34fc45d7c71
Paul Schwörer <hello@paulschwoerer.de>
2020-02-05 10:51:51 +0100

    Fix formatting in webpack.prod.js
    Signed-off-by: Paul Schwörer <hello@paulschwoerer.de>

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,16 +3,16 @@ const common = require('./webpack.common.js')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: '#source-map',
-    optimization: {
-        minimizer: [new TerserPlugin({
-            terserOptions: {
-                output: {
-                    comments: false,
-                }
-            },
-            sourceMap: true,
-        })],
-    }
-});
+	mode: 'production',
+	devtool: '#source-map',
+	optimization: {
+		minimizer: [new TerserPlugin({
+			terserOptions: {
+				output: {
+					comments: false,
+				},
+			},
+			sourceMap: true,
+		})],
+	},
+})

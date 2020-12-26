cee493c264e086f164f28d9595d1b85e9c496765
Julien Veyssier <eneiluj@posteo.net>
2020-12-26 18:52:48 +0100

    put content in track table
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/components/TrackTable.vue
+++ src/components/TrackTable.vue
@@ -16,6 +16,9 @@
 </template>
 
 <script>
+import moment from '@nextcloud/moment'
+
+import { metersToDistance, formatTimeSeconds, metersToElevation, kmphToSpeed, minPerKmToPace } from '../utils'
 
 export default {
 	name: 'TrackTable',
@@ -41,11 +44,59 @@ export default {
 			const lines = []
 			lines.push({
 				title: t('maps', 'Distance'),
-				value: meta.distance || '???',
+				value: meta.distance ? metersToDistance(meta.distance) : '???',
 			})
 			lines.push({
 				title: t('maps', 'Duration'),
-				value: meta.duration || '0',
+				value: formatTimeSeconds(meta.duration || 0),
+			})
+			lines.push({
+				title: t('maps', 'Moving time'),
+				value: formatTimeSeconds(meta.movtime || 0),
+			})
+			lines.push({
+				title: t('maps', 'Pause time'),
+				value: formatTimeSeconds(meta.stptime || 0),
+			})
+			lines.push({
+				title: t('maps', 'Begin'),
+				value: moment.unix(meta.begin).format('L HH:mm:ss (Z)'),
+			})
+			lines.push({
+				title: t('maps', 'End'),
+				value: moment.unix(meta.end).format('L HH:mm:ss (Z)'),
+			})
+			lines.push({
+				title: t('maps', 'Cumulative elevation gain'),
+				value: meta.posel ? metersToElevation(meta.posel) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Cumulative elevation loss'),
+				value: meta.negel ? metersToElevation(meta.negel) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Minimum elevation'),
+				value: meta.minel ? metersToElevation(meta.minel) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Maximum elevation'),
+				value: meta.maxel ? metersToElevation(meta.maxel) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Maximum speed'),
+				value: meta.maxspd ? kmphToSpeed(meta.maxspd) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Average speed'),
+				value: meta.avgspd ? kmphToSpeed(meta.avgspd) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Moving average speed'),
+				value: meta.movavgspd ? kmphToSpeed(meta.movavgspd) : 'NA',
+			})
+			lines.push({
+				title: t('maps', 'Moving average pace'),
+				value: meta.movpace ? minPerKmToPace(meta.movpace) : 'NA',
 			})
 			return lines
 		},

42c9c9e57b1a1ac4f69f408e52dabbd735795378
Julien Veyssier <eneiluj@posteo.net>
2020-12-26 23:38:51 +0100

    track table style
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/components/TrackTable.vue
+++ src/components/TrackTable.vue
@@ -1,10 +1,12 @@
 <template>
 	<div class="track-table">
-		{{ track.file_name }}
+		<h2>{{ track.file_name }}</h2>
 		<table class="track-table">
 			<tr v-for="l in tableLines"
 				:key="l.title">
 				<td>
+					<span v-if="l.iconClass" :class="'table-img-icon table-icon ' + l.iconClass" />
+					<span v-else :class="'table-icon ' + l.icon" />
 					{{ l.title }}
 				</td>
 				<td>
@@ -45,58 +47,72 @@ export default {
 			lines.push({
 				title: t('maps', 'Distance'),
 				value: meta.distance ? metersToDistance(meta.distance) : '???',
+				iconClass: 'distance',
 			})
 			lines.push({
 				title: t('maps', 'Duration'),
 				value: formatTimeSeconds(meta.duration || 0),
+				iconClass: 'clock',
 			})
 			lines.push({
 				title: t('maps', 'Moving time'),
 				value: formatTimeSeconds(meta.movtime || 0),
+				iconClass: 'clock',
 			})
 			lines.push({
 				title: t('maps', 'Pause time'),
 				value: formatTimeSeconds(meta.stptime || 0),
+				iconClass: 'clock',
 			})
 			lines.push({
 				title: t('maps', 'Begin'),
 				value: moment.unix(meta.begin).format('L HH:mm:ss (Z)'),
+				icon: 'icon-calendar-dark',
 			})
 			lines.push({
 				title: t('maps', 'End'),
 				value: moment.unix(meta.end).format('L HH:mm:ss (Z)'),
+				icon: 'icon-calendar-dark',
 			})
 			lines.push({
 				title: t('maps', 'Cumulative elevation gain'),
 				value: meta.posel ? metersToElevation(meta.posel) : 'NA',
+				iconClass: 'chart-line',
 			})
 			lines.push({
 				title: t('maps', 'Cumulative elevation loss'),
 				value: meta.negel ? metersToElevation(meta.negel) : 'NA',
+				iconClass: 'chart-line',
 			})
 			lines.push({
 				title: t('maps', 'Minimum elevation'),
 				value: meta.minel ? metersToElevation(meta.minel) : 'NA',
+				iconClass: 'chart-area',
 			})
 			lines.push({
 				title: t('maps', 'Maximum elevation'),
 				value: meta.maxel ? metersToElevation(meta.maxel) : 'NA',
+				iconClass: 'chart-area',
 			})
 			lines.push({
 				title: t('maps', 'Maximum speed'),
 				value: meta.maxspd ? kmphToSpeed(meta.maxspd) : 'NA',
+				iconClass: 'speed',
 			})
 			lines.push({
 				title: t('maps', 'Average speed'),
 				value: meta.avgspd ? kmphToSpeed(meta.avgspd) : 'NA',
+				iconClass: 'speed',
 			})
 			lines.push({
 				title: t('maps', 'Moving average speed'),
 				value: meta.movavgspd ? kmphToSpeed(meta.movavgspd) : 'NA',
+				iconClass: 'speed',
 			})
 			lines.push({
 				title: t('maps', 'Moving average pace'),
 				value: meta.movpace ? minPerKmToPace(meta.movpace) : 'NA',
+				iconClass: 'speed',
 			})
 			return lines
 		},
@@ -111,5 +127,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// plop
+.track-table {
+	width: 100%;
+	h2 {
+		text-align: center;
+	}
+	tr {
+		&:hover {
+			background-color: var(--color-background-hover);
+		}
+		td {
+			border: 1px solid var(--color-border);
+			padding: 0 5px 0 5px;
+		}
+	}
+
+	.distance {
+		mask: url('../../img/distance.svg') no-repeat;
+		-webkit-mask: url('../../img/distance.svg') no-repeat;
+	}
+	.clock {
+		mask: url('../../img/clock.svg') no-repeat;
+		-webkit-mask: url('../../img/clock.svg') no-repeat;
+	}
+	.speed {
+		mask: url('../../img/speed.svg') no-repeat;
+		-webkit-mask: url('../../img/speed.svg') no-repeat;
+	}
+	.chart-line {
+		mask: url('../../img/chart-line.svg') no-repeat;
+		-webkit-mask: url('../../img/chart-line.svg') no-repeat;
+	}
+	.chart-area {
+		mask: url('../../img/chart-area.svg') no-repeat;
+		-webkit-mask: url('../../img/chart-area.svg') no-repeat;
+	}
+	.table-icon {
+		display: inline-block;
+		min-width: 30px !important;
+		min-height: 16px !important;
+	}
+	.table-img-icon {
+		opacity: 1;
+		background-color: var(--color-main-text);
+		padding: 0 !important;
+		mask-size: 16px auto;
+		mask-position: center;
+		-webkit-mask-size: 16px auto;
+		-webkit-mask-position: center;
+	}
+}
 </style>

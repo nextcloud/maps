ea6a483f73fc3863d995f9baf3729727c1206792
kovge <kovger1986@gmail.com>
2021-04-11 21:02:37 +0200

    Fix for division by 0 errors
    - Fix Division by 0 Errors in case of incorrect Geocoordinate data
    - getExifPelBackup could throw any tipe of errors, all Throwable are Catched, to prevent service stop
    
    Signed-off-by: Gergely Kovács <kovger1986@gmail.com>

--- lib/Service/PhotofilesService.php
+++ lib/Service/PhotofilesService.php
@@ -382,7 +382,7 @@ class PhotofilesService {
         if (!$this->hasValidExifGeoTags($exif)) {
             try {
                 @$exif = $this->getExifPelBackup($file);
-            } catch (PelException $exception) {
+            } catch (Throwable $exception) {
                 $exif = [];
             }
 
@@ -410,7 +410,7 @@ class PhotofilesService {
                 $pos = strpos($value, '/');
                 if ($pos !== false){
                     $temp = explode('/',$value);
-                    $gps[$key] = $temp[0] / $temp[1];
+                    $gps[$key] = ($temp[1] == 0) ? 0 : ($temp[0] / $temp[1]);
                 }
             }
             $file_object = new \stdClass();

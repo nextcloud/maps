e1c56c0ba607755992c2f887866b0fce3365a109
Gergely Kovács <kovger1986@gmail.com>
2021-04-11 21:47:19 +0200

    fix division by 0 error
    Signed-off-by: Gergely Kovács <kovger1986@gmail.com>
    Signed-off-by: schklom <23263086+schklom@users.noreply.github.com>

--- lib/Service/PhotofilesService.php
+++ lib/Service/PhotofilesService.php
@@ -424,7 +424,7 @@ class PhotofilesService {
                 $pos = strpos($value, '/');
                 if ($pos !== false){
                     $temp = explode('/',$value);
-                    $gps[$key] = ($temp[1] == 0) ? 0 : ($temp[0] / $temp[1]);
+                    $gps[$key] = ($temp[1] == 0) ? $temp[0] : ($temp[0] / $temp[1]);
                 }
             }
             $file_object = new \stdClass();

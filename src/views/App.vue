6c0d881d3faaf850c5fe9e33c03ff8541f828d52
Arne Hamann <kontakt+github@arne.email>
2022-03-21 09:42:34 +0100

    Nicer photo sidebar
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

--- src/views/App.vue
+++ src/views/App.vue
@@ -197,7 +197,7 @@ export default {
 		return {
 			optionValues: optionsController.optionValues,
 			sendPositionTimer: null,
-			showSidebar: true,
+			showSidebar: false,
 			sidebarIsFullScreen: false,
 			activeSidebarTab: '',
 			// slider

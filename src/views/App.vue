b9977e479df5772e78125df1f6d8e3781b53283c
Arne Hamann <kontakt+github@arne.email>
2022-03-21 12:12:45 +0100

    Load photo Suggestions when navigation icon is clicked
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

--- src/views/App.vue
+++ src/views/App.vue
@@ -39,10 +39,12 @@
 					:loading="photosLoading"
 					:photos="photos"
 					:draggable="photosDraggable"
+					:show-suggestions="showPhotoSuggestions"
 					@photos-clicked="onPhotosClicked"
 					@cancel-clicked="cancelPhotoMove"
 					@redo-clicked="redoPhotoMove"
-					@draggable-clicked="photosDraggable = !photosDraggable" />
+					@draggable-clicked="photosDraggable = !photosDraggable"
+					@suggestions-clicked="onPhotoSuggestionsClicked" />
 				<AppNavigationTracksItem
 					ref="tracksNavigation"
 					:enabled="tracksEnabled"
@@ -171,6 +173,7 @@ import { processGpx } from '../tracksUtils'
 import L from 'leaflet'
 import { geoToLatLng, getFormattedADR } from '../utils/mapUtils'
 import * as network from '../network'
+import { getPhotoSuggestions } from '../network'
 
 export default {
 	name: 'App',
@@ -220,6 +223,8 @@ export default {
 			photosDraggable: false,
 			photos: [],
 			selectedPhoto: null,
+			showPhotoSuggestions: false,
+			photoSuggestions: [],
 			// contacts
 			contactsLoading: false,
 			contactsEnabled: optionsController.contactsEnabled,
@@ -695,6 +700,12 @@ export default {
 			}
 			optionsController.saveOptionValues({ photosLayer: this.photosEnabled ? 'true' : 'false' })
 		},
+		onPhotoSuggestionsClicked() {
+			this.showPhotoSuggestions = !this.showPhotoSuggestions
+			if (this.photosEnabled && this.showPhotoSuggestions && this.photoSuggestions.length === 0) {
+				this.getPhotoSuggestions()
+			}
+		},
 		getPhotos() {
 			if (!this.photosEnabled) {
 				return
@@ -702,6 +713,26 @@ export default {
 			this.photosLoading = true
 			network.getPhotos().then((response) => {
 				this.photos = response.data.sort((a, b) => {
+					if (a.dateTaken < b.dateTaken) {
+						return -1
+					} else if (a.dateTaken > b.dateTaken) {
+						return 1
+					}
+					return 0
+				})
+			}).catch((error) => {
+				console.error(error)
+			}).then(() => {
+				this.photosLoading = false
+			})
+		},
+		getPhotoSuggestions() {
+			if (!this.photosEnabled) {
+				return
+			}
+			this.photosLoading = true
+			network.getPhotoSuggestions().then((response) => {
+				this.photoSuggestions = response.data.sort((a, b) => {
 					if (a.dateTaken < b.dateTaken) {
 						return -1
 					} else if (a.dateTaken > b.dateTaken) {

fd78ee73a882b0154c7d708613ad2cd22a497bdf
Arne Hamann <kontakt+github@arne.email>
2022-03-23 11:11:17 +0100

    Migrated to new App Bootstrap
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * ownCloud - maps
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Sander Brand <brantje@gmail.com>, Vinzenz Rosenkranz <vinzenz.rosenkranz@gmail.com>
+ * @copyright Sander Brand 2014, Vinzenz Rosenkranz 2016, 2017
+ */
+
+namespace OCA\Maps\AppInfo;
+
+
+use \OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use \OCP\IServerContainer;
+use OCA\Maps\Hooks\FileHooks;
+use OCA\Maps\Service\PhotofilesService;
+use OCA\Maps\Service\TracksService;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCA\Files\Event\LoadAdditionalScriptsEvent;
+use OCA\DAV\Events\CardCreatedEvent;
+use OCA\DAV\Events\CardUpdatedEvent;
+use OCA\DAV\Events\CardDeletedEvent;
+use OCA\Maps\Listener\LoadAdditionalScriptsListener;
+use OCA\Maps\Listener\CardCreatedListener;
+use OCA\Maps\Listener\CardUpdatedListener;
+use OCA\Maps\Listener\CardDeletedListener;
+
+
+class Application extends App implements IBootstrap {
+    public function __construct (array $urlParams=array()) {
+        parent::__construct('maps', $urlParams);
+    }
+
+	public function register(IRegistrationContext $context): void {
+		// ... registration logic goes here ...
+
+		// Register the composer autoloader for packages shipped by this app, if applicable
+		include_once __DIR__ . '/../../vendor/autoload.php';
+		$context->registerEventListener(
+			LoadAdditionalScriptsEvent::class,
+			LoadAdditionalScriptsListener::class
+		);
+
+		$context->registerEventListener(
+			CardCreatedEvent::class,
+			CardCreatedListener::class
+		);
+		$context->registerEventListener(
+			CardUpdatedEvent::class,
+			CardUpdatedListener::class
+		);
+		$context->registerEventListener(
+			CardDeletedEvent::class,
+			CardDeletedListener::class
+		);
+	}
+
+	public function boot(IBootContext $context): void {
+		// ... boot logic goes here ...
+		$context->getAppContainer()->registerService('FileHooks', function($c) {
+			return new FileHooks(
+				$c->query(IServerContainer::class)->getRootFolder(),
+				\OC::$server->query(PhotofilesService::class),
+				\OC::$server->query(TracksService::class),
+				$c->query(IServerContainer::class)->getLogger(),
+				$c->query('AppName'),
+				$c->query(IServerContainer::class)->getLockingProvider()
+			);
+		});
+
+		$context->getAppContainer()->query('FileHooks')->register();
+
+		$this->registerFeaturePolicy();
+	}
+
+	private function registerFeaturePolicy() {
+		$dispatcher = $this->getContainer()->getServer()->getEventDispatcher();
+
+		$dispatcher->addListener('OCP\Security\FeaturePolicy\AddFeaturePolicyEvent', function (\OCP\Security\FeaturePolicy\AddFeaturePolicyEvent $e) {
+			$fp = new \OCP\AppFramework\Http\EmptyFeaturePolicy();
+			$fp->addAllowedGeoLocationDomain('\'self\'');
+			$e->addPolicy($fp);
+		});
+	}
+
+}

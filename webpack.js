83b162cefdb963d6b8cca2fd10aad60f227c7cb9
Arne Hamann <git@arne.email>
2023-02-10 20:22:57 +0100

    Add copy link to map share action
    Signed-off-by: Arne Hamann <git@arne.email>

--- webpack.js
+++ webpack.js
@@ -15,6 +15,7 @@ webpackConfig.entry = {
 	// script: { import: path.join(__dirname, 'src', 'script.js'), filename: 'maps-script.js' },
 	main: { import: path.join(__dirname, 'src', 'main.js'), filename: 'maps-main.js' },
 	'track-metadata-tab': { import: path.join(__dirname, 'src', 'track-metadata-tab.js'), filename: 'maps-track-metadata-tab.js' },
+	'external-share-map': { import: path.join(__dirname, 'src', 'external-share-map.js'), filename: 'maps-external-share-map.js' },
 	filetypes: { import: path.join(__dirname, 'src', 'filetypes.js'), filename: 'maps-filetypes.js' },
 	'public-favorite-share': { import: path.join(__dirname, 'src', 'publicFavoriteShare.js'), filename: 'maps-publicFavoriteShare.js' },
 }

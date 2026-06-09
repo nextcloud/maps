d467bb6de7082a674991a10685bbc198aff59ed5
Carl Schwan <carlschwan@kde.org>
2026-06-09 15:10:46 +0200

    chore: Add rector
    For now only for the tests
    
    Signed-off-by: Carl Schwan <carlschwan@kde.org>

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+use Nextcloud\Rector\Set\NextcloudSets;
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+
+return RectorConfig::configure()
+	->withPaths([
+		#__DIR__ . '/lib',
+		__DIR__ . '/tests/Unit',
+	])
+	->withSkip([
+		__DIR__ . '/tests/stubs',
+	])
+	->withPreparedSets(
+		deadCode: true,
+		typeDeclarations: true,
+	)->withSets([
+		NextcloudSets::NEXTCLOUD_30,
+	])->withPhpSets(
+		php82: true,
+	)->withConfiguredRule(ClassPropertyAssignToConstructorPromotionRector::class, [
+		'inline_public' => true,
+		'rename_property' => true,
+	]);
